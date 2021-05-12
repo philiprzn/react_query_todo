3e52a264521c83470dc95b08f9ed181d8ff37590
philip_maksyutov <philip_bmx11@yahoo.com>
2021-05-12 21:16:20 +0300

    Checkbox click optimization;

--- react-query-todo/src/components/TodoItem.js
+++ react-query-todo/src/components/TodoItem.js
@@ -6,7 +6,35 @@ export const TodoItem = ({id, text, isCompleted }) => {
     const queryCache = useQueryCache();
 
     const [mutateCheck] = useMutation(updateTodo, {
-        onSuccess: () => queryCache.invalidateQueries('todos')
+        // onSuccess: () => queryCache.invalidateQueries('todos')
+
+        onMutate: (newTodo) => {
+            queryCache.cancelQueries("todos");
+            const previousQuery = queryCache.getQueryData("todos");
+
+            queryCache.setQueryData("todos", (oldQuery) => {
+                return oldQuery.map(group => {
+                    return {
+                        ...group,
+                        record: group.records.map(rec => {
+                            if(rec.id === newTodo.id) {
+                                return {
+                                    ...rec,
+                                    fields: {...rec.fields, ...newTodo.fields}
+                                }
+                            } else {
+                                return rec;
+                            }
+                        })
+                    }
+                })
+            })
+            return () => queryCache.setQueryData("todos", previousQuery);
+        },
+        onError: (err, newTodo, rollback) => rollback(),
+        onSettled: (newTodo) => {
+            queryCache.invalidateQueries('todos')
+        }
     });
 
     const [mutateDelete] = useMutation(deleteTodo, {
